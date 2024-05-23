d583b456694e4cd0e25640d5d9e83753dce6cf3e
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-05-23 14:12:58 +0200

    Correction Bonus Build

--- Exercice-Build/build.mjs
+++ Exercice-Build/build.mjs
@@ -1,7 +1,10 @@
 import fs from "node:fs/promises";
 import { resolve } from "node:path";
-import md5 from "md5";
+import { createHash } from "node:crypto";
 import { minify } from "terser";
+import minimist from "minimist";
+
+const argv = minimist(process.argv);
 
 const distPath = resolve(import.meta.dirname, "dist");
 const srcPath = resolve(import.meta.dirname, "src");
@@ -9,7 +12,7 @@ const horlogeJsPath = resolve(srcPath, "js", "horloge.js");
 const indexJsPath = resolve(srcPath, "js", "index.js");
 const indexHtmlPath = resolve(srcPath, "index.html");
 const indexHtmlDistPath = resolve(distPath, "index.html");
-const appJsDistPath = resolve(distPath, "app.js");
+let appJsDistPath = resolve(distPath, "app.js");
 
 async function emptyDir(dirPath) {
   await fs.rm(dirPath, { recursive: true, force: true });
@@ -17,22 +20,36 @@ async function emptyDir(dirPath) {
 }
 
 async function buildJs() {
-  const horlogeBuffer = await fs.readFile(horlogeJsPath);
-  await fs.appendFile(appJsDistPath, horlogeBuffer);
+  const buffers = await Promise.all([
+    fs.readFile(horlogeJsPath),
+    fs.readFile(indexJsPath),
+  ]);
 
-  const indexBuffer = await fs.readFile(indexJsPath);
-  await fs.appendFile(appJsDistPath, indexBuffer);
+  let hash = null;
+  let bufferOrStr = Buffer.concat(buffers);
 
-  return "abc";
+  if (argv.minify) {
+    const { code } = await minify(bufferOrStr.toString('utf-8'));
+    bufferOrStr = code;
+  }
+
+  if (argv.hash) {
+    hash = createHash('md5').update(bufferOrStr).digest('hex');
+    appJsDistPath = appJsDistPath.replace('.js', `.${hash}.js`);
+  }
+
+  await fs.writeFile(appJsDistPath, bufferOrStr);
+
+  return hash;
 }
 
-async function buildHtml() {
+async function buildHtml(hash) {
   let indexStr = await fs.readFile(indexHtmlPath, { encoding: "utf-8" });
 
   indexStr = indexStr
     .replace(
       '<script src="./js/horloge.js"></script>',
-      '<script src="./app.js"></script>'
+      hash ? `<script src="./app.${hash}.js"></script>` : '<script src="./app.js"></script>'
     )
     .replace('<script src="./js/index.js"></script>', "");
 
@@ -40,5 +57,5 @@ async function buildHtml() {
 }
 
 await emptyDir(distPath);
-const valABC = await buildJs();
-await buildHtml();
+const hash = await buildJs();
+await buildHtml(hash);
